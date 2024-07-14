8665e1bc73d415be6e6dfa98de975e4fc85e0e8c
Simon Hauser <Simon-Hauser@outlook.de>
2024-07-14 15:19:54 +0200

    feat: extend timing_measurement interface to support multiple keys

--- core/src/control/diagnostic_tool/timing_measurement.cpp
+++ core/src/control/diagnostic_tool/timing_measurement.cpp
@@ -3,30 +3,42 @@
 #include <stdlib.h>
 
 namespace Control {
-std::map<int32_t, TimingMeasurement::Measurement>
+std::map<int32_t, std::map<std::string, TimingMeasurement::Measurement>>
     TimingMeasurement::measurements_;
 
-void TimingMeasurement::start(int32_t timestep) {
-  auto iter = measurements_.find(timestep);
+void TimingMeasurement::start(int32_t timestep, const char *name) {
+  auto innerMap = measurements_.find(timestep);
 
-  if (iter == measurements_.end()) {
+  if (innerMap == measurements_.end()) {
     auto insertedIter = measurements_.insert(
-        std::pair<int32_t, Measurement>(timestep, Measurement{}));
-    iter = insertedIter.first;
-    iter->second.start = MPI_Wtime();
-    iter->second.set = false;
+        std::pair<int32_t, std::map<std::string, Measurement>>(timestep, {}));
+    innerMap = insertedIter.first;
+  }
+  if (innerMap->second.find(name) == innerMap->second.end()) {
+    innerMap->second.insert(std::pair<std::string, Measurement>(
+        name, Measurement({MPI_Wtime(), -1, false})));
   }
 }
 
-void TimingMeasurement::stop(int32_t timestep) {
+void TimingMeasurement::stop(int32_t timestep, const char *name) {
   double stopTime = MPI_Wtime();
-  if (measurements_.find(timestep) != measurements_.end()) {
-    Measurement &measurement = measurements_[timestep];
-    if (!measurement.set) {
-      measurement.duration = stopTime - measurement.start;
-      measurement.set = true;
-      TimingMeasurement::writeCSV();
-    }
+  if (measurements_.find(timestep) == measurements_.end()) {
+    return;
+  }
+  auto &timeStepMeasurements = measurements_[timestep];
+  if (timeStepMeasurements.find(name) == timeStepMeasurements.end()) {
+    return;
+  }
+  auto &measurement = timeStepMeasurements[name];
+  if (!measurement.set) {
+    measurement.duration = stopTime - measurement.start;
+    LOG(INFO) << "[]: " << name
+              << " setting duration: " << measurement.duration;
+    measurement.set = true;
+  }
+
+  if (strcmp(name, "all") == 0) {
+    TimingMeasurement::writeCSV();
   }
 }
 
@@ -46,11 +58,25 @@ void TimingMeasurement::writeCSV(const char *filename) {
   std::stringstream header;
   std::stringstream data;
 
-  header << "timestep;duration" << std::endl;
-  for (const std::pair<int32_t, Measurement> m : measurements_) {
-    if (m.second.set) {
-      data << m.first << ";" << m.second.duration << std::endl;
+  bool headerSet = false;
+  for (const auto &tms : measurements_) {
+    if (!headerSet) {
+      header << "timestep";
+      for (const auto &m : tms.second) {
+        header << ";" << m.first;
+      }
+      header << std::endl;
+      headerSet = true;
     }
+
+    data << tms.first;
+    for (const auto &m : tms.second) {
+      data << ";";
+      if (m.second.set) {
+        data << m.second.duration;
+      }
+    }
+    data << std::endl;
   }
 
   std::ofstream out(ss.str());
