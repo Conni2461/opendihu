a7bd6237cc5010034b9933a94f9b4d7abb4416d7
Simon Hauser <simon.hauser@helsinki-systems.de>
2024-07-30 19:05:56 +0200

    feat: more checkpointing comments

--- core/src/checkpointing/paraview/combined.h
+++ core/src/checkpointing/paraview/combined.h
@@ -8,22 +8,29 @@
 
 namespace Checkpointing {
 namespace Paraview {
+//! The concreate implementation for Paraview combined checkpointing
 class Combined : public Generic {
 public:
+  //! Constructs a new combined Paraview checkpointing implementation
   Combined(DihuContext context,
            std::shared_ptr<Partition::RankSubset> rankSubset = nullptr,
            const std::string &prefix = ".");
 
+  //! Create a new checkpoint with the problem data as well as the timestep and
+  //! the current time.
   template <typename DataType>
   void createCheckpoint(DataType &problemData, int timeStepNo = -1,
                         double currentTime = 0.0) const;
+  //! Currently not implemented. Will not restore a checkpoint
   template <typename DataType>
   bool restore(DataType &problemData, int &timeStepNo, double &currentTime,
                bool autoRestore,
                const std::string &checkpointingToRestore) const;
 
 private:
-  std::unique_ptr<OutputWriter::Paraview> writer_;
+  std::unique_ptr<OutputWriter::Paraview>
+      writer_; //< Constructed output writer that is used to create new
+               //checkpoints
 };
 } // namespace Paraview
 } // namespace Checkpointing
